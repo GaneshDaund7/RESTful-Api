81ad6c50c887941dbe839498f97027153563a1ef
Ganesh Daund <daundganesh496@gmail.com>
2021-06-08 17:42:45 +0530

    Upto 5th module

---
+++ Profiles/AuthorProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CourseLibrary.API.Helpers;
+
+namespace CourseLibrary.api.Profiles
+{
+    public class AuthorProfile: Profile
+    {
+        public AuthorProfile()
+        {
+            CreateMap<API.Entities.Author, Models.AuthorsDto>()
+               .ForMember(
+                   dest => dest.Name,
+                   opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+               .ForMember(
+                   dest => dest.Age,
+                   opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
+            ;
+        }
+    }
+}
